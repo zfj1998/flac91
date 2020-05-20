1eb5c4fe93208f1bc03d2dc75acaa9895f1ea890
jven <893298592@qq.com>
2020-05-20 15:59:22 +0800

    changed edition workable

--- flac91/items.py
+++ flac91/items.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+import os
+import logging
+from scrapy.utils.project import get_project_settings
+
+settings = get_project_settings()
+ROOT_PATH = settings.get('DOWNLOAD_ROOT_PATH')
+QUALITYS = settings.get('QUALITYS')
+QUALITYS_FORMAT = settings.get('QUALITYS_FORMAT') # 用于文件存储格式
 
 class SongItem(scrapy.Item):
     '''
@@ -19,3 +21,64 @@ class SongItem(scrapy.Item):
     download = scrapy.Field() #下载链接
     file_format = scrapy.Field() #文件格式
     cookie = scrapy.Field() # cookie jar用于pipeline下载
+
+    def trim_file_name(self, name):
+        illegal = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
+        for i in illegal:
+            name = name.replace(i, '')
+        return name
+
+    def get_directory(self):
+        directory = os.path.join(
+            ROOT_PATH,
+            self['singer'],
+            self.trim_file_name(self['album'])
+        )
+        return directory
+
+    def get_song_path(self):
+        directory = self.get_directory()
+        file_name = '{name}.{form}'.format(
+            name=self.trim_file_name(self['song']),
+            form=self['file_format']
+        )
+        song_path = os.path.join(directory, file_name)
+        return song_path
+    
+    def get_lyrics_path(self):
+        directory = self.get_directory()
+        lyrics_file_name = '{name}.txt'.format(
+            name=self.trim_file_name(self['song'])
+        )
+        lyrics_file_path = os.path.join(directory, lyrics_file_name)
+        return lyrics_file_path
+
+    def check_directory(self):
+        directory = self.get_directory()
+        if not os.path.isdir(directory):
+            try:
+                os.makedirs(directory)
+                self['directory'] = directory
+            except Exception as e:
+                logging.error('Error while create directory {}'.format(e))
+        return True
+
+    def song_file_existed(self):
+        result = False
+        for f in QUALITYS:
+            self['file_format'] = QUALITYS_FORMAT[f]
+            file_path = self.get_song_path()
+            result = os.path.exists(file_path)
+            if result:
+                break
+        if result:
+            logging.info('Song:{name} already exists at {path}'.format(name=self['song'], path=file_path))
+        return result
+    
+    def lyrics_file_existed(self):
+        lyrics_path = self.get_lyrics_path()
+        result = os.path.exists(lyrics_path)
+        if result:
+            logging.info('Lyrics:{name} already exists at {path}'.format(name=self['song'], path=lyrics_path))
+        return result
+    
--- flac91/pipelines.py
+++ flac91/pipelines.py
@@ -10,65 +10,29 @@ import requests
 from scrapy.pipelines.files import FilesPipeline
 from scrapy import Request
 
-DOWNLOAD_ROOT_PATH = r'C:\Users\Jven\Desktop\Music'
-
-
 class DownloadPipeline(object):
     '''
     自定义音乐下载
     '''
-    # def get_media_requests(self, item, info):
-    #     logging.info('{}-{}'.format(item['song'],item['download']))
-    #     if item['download']:
-    #         yield Request(item['download'], meta={'item': item})
-    
-    # def file_path(self, request, response=None, info=None):
-    #     item = response.meta['item']
-    #     if not item:
-    #         return
-    #     directory = os.path.join(DOWNLOAD_ROOT_PATH, item['singer'], item['album'])
-    #     if not os.path.isdir(directory):
-    #         os.makedirs(directory)
-    #     file_name = '{name}.{form}'.format(name=item['song'], form=item['file_format'])
-    #     file_path = os.path.join(directory, file_name)
-    #     logging.info('Song:{name} downloaded to {path}'.format(name=item['song'], path=file_path))
-    #     return file_path
-    def trim_file_name(self, name):
-        illegal = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
-        for i in illegal:
-            name = name.replace(i, '')
-        return name
-
     def process_item(self, item, spider):
         # 下载音乐文件
         if not item['download']:
             logging.info('Song:{name}-{album} has no download url'.format(name=item['song'], album=item['album']))
             return item
-        item['song'] = self.trim_file_name(item['song'])
-        item['album'] = self.trim_file_name(item['album'])
-        # logging.info('Song:{name} download_url:{url}'.format(name=item['song'], url=item['download']))
+        #下载文件
         response = requests.get(item['download'],headers=item['cookie'])
         file_content = response.content
-        directory = os.path.join(DOWNLOAD_ROOT_PATH, item['singer'], item['album'])
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
-        file_name = '{name}.{form}'.format(name=item['song'], form=item['file_format'])
-        file_path = os.path.join(directory, file_name)
-        if os.path.exists(file_path):
-            logging.info('Song:{name} already exists at {path}'.format(name=item['song'], path=file_path))
-        else:  
-            with open(file=file_path, mode='wb') as f:
-                f.write(file_content)
-            logging.info('Song:{name} downloaded to {path}'.format(name=item['song'], path=file_path))
+        
+        file_path = item.get_song_path()
+        with open(file=file_path, mode='wb') as f:
+            f.write(file_content)
+        logging.info('Song:{name} downloaded to {path}'.format(name=item['song'], path=file_path))
         
         # 储存歌词文件
         if not item['lyrics']:
             return item
-        lyrics_file_name = '{name}.txt'.format(name=item['song'])
-        lyrics_file_path = os.path.join(directory, lyrics_file_name)
-        if os.path.exists(lyrics_file_path):
-            logging.info('Lyrics:{name} already exists at {path}'.format(name=item['song'], path=file_path))
-        else: 
+        lyrics_file_path = item.get_lyrics_path()
+        if not item.lyrics_file_existed():
             with open(file=lyrics_file_path, mode='w', encoding='utf-8') as f:
                 f.write(item['lyrics'])
             logging.info('Lyrics:{name} downloaded to {path}'.format(name=item['song'], path=lyrics_file_path))
